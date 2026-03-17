be9d132f07a1cab84de27c2c1e4d9d6d70a3dfc4
francisco-souto <francisco.souto@mj.gov.br>
2026-03-16 21:14:14 -0300

    ONASP Inspecoes PWA v1.0

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
-const base = process.env.GITHUB_PAGES === 'true' ? '/onasp-inspecoes/' : '/'
+const base = process.env.GITHUB_PAGES === 'true' ? '/EMA/' : '/'
 
 export default defineConfig({
   base,
